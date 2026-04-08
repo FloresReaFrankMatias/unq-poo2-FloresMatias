67d85f51880e984b8cf436b652ec242f578e9649
FloresReaFrankMatias <matiasfloresrea@gmail.com>
2026-04-08 18:06:25 -0300

    add class abstact empleado

---
+++ src/ar/edu/unq/poo2/tp2/Empleado.java
@@ -0,0 +1,55 @@
+package ar.edu.unq.poo2.tp2;
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Empleado {
+	private String nombre;
+	private String direccion;
+	private LocalDate fechaNacimiento;
+	private float sueldoBasico;
+	private String estadoCivil;
+	
+
+	public Empleado(String nombre, String direccion, LocalDate fechaNacimiento, float sueldoBasico, String estadoCivil){
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.fechaNacimiento = fechaNacimiento;
+		this.sueldoBasico= sueldoBasico;
+		this.estadoCivil = estadoCivil;
+	}
+	
+	public int getEdad() {
+		return Period.between(this.fechaNacimiento, LocalDate.now()).getYears(); 
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	 
+	//public float sueldoBruto() {
+		//return sueldoBasico + sueldoFamiliar()
+	//}
+	public abstract float sueldoBruto();
+	
+	public float sueldoNeto() {
+		return this.sueldoBruto() - this.retenciones();
+	}
+	public float retenciones() {
+		return this.obraSocial() + this.aportesJubilatorios() + this.adicionales();
+	}
+	
+	public float obraSocial() {
+		return this.sueldoBruto() * 0.10f; // la f es porque es float, preguntar
+	}
+	public float aportesJubilatorios() {
+		return this.sueldoBruto() * this.porcentajeDeAporteJubilatorio();
+	}
+	public abstract float adicionales();
+	public abstract float porcentajeDeAporteJubilatorio();
+
+	
+
+
+}
