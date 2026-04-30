9896355b02659d456c6923289f7db87b187cc270
FloresReaFrankMatias <matiasfloresrea@gmail.com>
2026-04-30 12:18:38 -0300

    add interface Agencia

---
+++ src/ar/edu/unq/poo2/tp5/Caja_MercadoCentral_v2/Agencia.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.poo2.tp5.Caja_MercadoCentral_v2;
+
+import ar.edu.unq.poo2.tp5.Caja_MercadoCentral_v2.Facturas.Factura;
+
+public interface Agencia {
+	public void registrarPago(Factura factura );
+}
