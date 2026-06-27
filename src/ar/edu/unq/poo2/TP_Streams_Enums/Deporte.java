b7610ae5b179b927ae170bdb537d2375ef051b7c
FloresReaFrankMatias <matiasfloresrea@gmail.com>
2026-06-27 18:28:23 -0300

    added Enum Deporte

---
+++ src/ar/edu/unq/poo2/TP_Streams_Enums/Deporte.java
@@ -0,0 +1,17 @@
+package ar.edu.unq.poo2.TP_Streams_Enums;
+
+public enum Deporte {
+    RUNNING(1),
+    FUTBOL(2),
+    BASKET(2),
+    TENNIS(3),
+    JABALINA(4);
+
+    private final int complejidad;
+
+    Deporte(int complejidad) {
+        this.complejidad = complejidad;
+    }
+
+    public int getComplejidad() { return complejidad; }}
+
